8cd3bf84c025bb31090057ee4a90029936d2173c
ivanleongwm <ivanleongwm@gmail.com>
2022-04-02 10:59:20 +0800

    push

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const HolidayController = require("./controllers/holidayController")
+
+const app = express();
+const PORT = process.env.PORT ?? 2000
+const MONGODB_URI = process.env.MONGODB_URI ?? "mongodb://localhost:27017/holidays"
+
+// Error / Disconnection
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+
+//...farther down the page
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose...");
+});
+
+app.use("/api/holidays", HolidayController)
+
+
+app.get("/", (req, res) => {
+    res.send('Hi 2');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
